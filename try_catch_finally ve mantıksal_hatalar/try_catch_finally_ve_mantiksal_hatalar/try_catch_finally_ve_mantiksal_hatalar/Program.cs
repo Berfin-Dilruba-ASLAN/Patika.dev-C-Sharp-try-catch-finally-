8ba71d70f1e41763c0e28fa3dd2f378c205559c7
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-17 10:09:41 +0300

    Add files via upload

---
+++ try_catch_finally ve mantıksal_hatalar/try_catch_finally_ve_mantiksal_hatalar/try_catch_finally_ve_mantiksal_hatalar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace try_catch_finally_ve_mantiksal_hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("HATA: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı. ");
+            }
+        }
+    }
+}
